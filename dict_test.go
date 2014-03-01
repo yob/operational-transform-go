b4b63267d47a3a8d823478c9aced6972df743cda
James Healy <james@yob.id.au>
2014-03-01 21:14:57 +1100

    remove unused hash function
    * it was required when versions were addressed using a checksum, now we
      address versions using an int

--- dict_test.go
+++ dict_test.go
@@ -4,26 +4,6 @@ import (
 	"testing"
 )
 
-func TestDictHashing(t *testing.T) {
-	dict := Dict{
-		"doc": "Haha this is is some text",
-	}
-	h := hash(dict)
-	target := "\xf7\xf0)칊\xbe\x97\x90t\xa3\xabE\xb7M\xbd\x9a\xf0-B"
-	if h != target {
-		t.Errorf("Hash failed, got %x, instead of %x", h, target)
-	}
-
-	dict = Dict{
-		"titi": "toto",
-	}
-	h = hash(dict)
-	target = "\xf7眨\xeb\v1\xeeM]l\x18\x14\x16f\u007f\xfe\xe5(\xed"
-	if h != target {
-		t.Errorf("Hash failed, got %x, instead of %x", h, target)
-	}
-}
-
 func TestDictGetting(t *testing.T) {
 	dict := Dict{
 		"doc": "Haha this is is some text",
--- ot.go
+++ ot.go
@@ -1,40 +1,5 @@
 package sharego
 
-import (
-	"crypto/sha1"
-	"fmt"
-	"io"
-	"unsafe"
-)
-
-//hashes a Dict, to produce checksums used within Document struct. hashes reflects
-//the whole dict, both values and keys to be unique for each document.
-func hash(content Dict) string {
-	h := sha1.New()
-	for key, val := range content {
-		io.WriteString(h, key)
-		switch value := val.(type) {
-		case Dict:
-			io.WriteString(h, hash(value))
-		case []interface{}:
-			for _, el := range value {
-				switch element := el.(type) {
-				case Dict:
-					io.WriteString(h, hash(element))
-				case fmt.Stringer:
-					io.WriteString(h, element.String())
-				}
-
-			}
-		case fmt.Stringer:
-			io.WriteString(h, value.String())
-		case unsafe.Pointer:
-			io.WriteString(h, *(*string)(value))
-		}
-	}
-	return string(h.Sum(nil))
-}
-
 //Given the old position of an insert operation returns its new position
 //when transforming against another component.
 func transformPosition(oldpos int, comp Component) (newpos int) {
