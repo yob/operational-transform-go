2ec5251231de445b4ba8b4452feb68322ab3a59b
James Healy <james@yob.id.au>
2014-03-01 21:16:05 +1100

    move a private util method into util.go
    * group the util methods together

--- util.go
+++ util.go
@@ -18,3 +18,25 @@ func pathEquals(strslice1, strslice2 []string) (b bool) {
 	return
 }
 
+//Given the old position of an insert operation returns its new position
+//when transforming against another component.
+func transformPosition(oldpos int, comp Component) (newpos int) {
+	newpos = oldpos
+	compos := comp.position()
+	if comp.Si != "" {
+		if compos <= oldpos {
+			newpos += len(comp.Si)
+		}
+	} else {
+		if oldpos <= compos {
+			newpos = oldpos
+		} else if oldpos <= compos+len(comp.Sd) {
+			newpos = compos
+		} else {
+			newpos = oldpos - len(comp.Sd)
+		}
+	}
+	return
+
+}
+
