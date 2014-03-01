53843ff076a300ec28b42f2ee309bf03504b0d5c
James Healy <james@yob.id.au>
2014-03-01 22:23:11 +1100

    experiment with delaying operation transfrom until read-time
    * applying the operations just orders and stores them in the document
    * When Document.Snapshot() is called, the operations are run in sequence
      to generate the requested document version
    * This is be slower for reads, but more flexible
    * The slowness can be resolved by persisting a copy of the document every
      20-30 versions. I'll get to that once I have persistence.

--- document.go
+++ document.go
@@ -3,15 +3,15 @@ package sharego
 //This is document type. Dict is a very flexible struct to contain python like
 //dict structure. For now only supports string inserts and deletes.
 type Document struct {
-	content   Dict
+	initial   Dict
 	ops       []Operation
 }
 
 //Returns a new document containing initialized map, from dict.
-func NewDocument(content Dict) (doc Document) {
+func NewDocument(initial Dict) (doc Document) {
 	doc = Document{
 		ops: make([]Operation, 0),
-		content: content,
+		initial: initial,
 	}
 	return
 }
@@ -22,8 +22,47 @@ func (doc Document) Version() int {
 }
 
 // Returns the latest version of the document
-func (doc *Document) Snapshot() Dict {
-	return doc.content
+func (doc *Document) Snapshot() (Dict, error) {
+	ops := doc.ops[0:doc.Version()]
+	result, err := transform(doc.initial, ops)
+	if (err != nil) {
+		return nil, err
+	}
+	return result, nil
+}
+
+// Starting with an initial Dict, apply 0 or more operations to it and return
+// the result.
+func transform(content Dict, ops []Operation) (Dict, error) {
+	for _, op := range ops {
+		for c := 0; c < len(op); c++ {
+			comp := op[c]
+			if comp.Si != "" {
+				index := comp.position()
+				str, err := content.get(comp.Path[:len(comp.Path)-1])
+				if err != nil {
+					return nil, InvalidComponentError{msg: str}
+				}
+				str = str[:index] + comp.Si + str[index:]
+				content.set(comp.Path[:len(comp.Path)-1], str)
+			}
+			if comp.Sd != "" {
+				str, err := content.get(comp.Path[:len(comp.Path)-1])
+				if err != nil {
+					return nil, err
+				}
+				str_length := len(comp.Sd)
+				index := comp.position()
+				deleted := str[index : index+str_length]
+				if deleted != comp.Sd {
+					return nil, InvalidComponentError{"Trying to delete '" + comp.Sd + "' but found '" + deleted + "' instead"}
+				}
+				new_str := str[:index] + str[index+str_length:]
+				content.set(comp.Path[:len(comp.Path)-1], new_str)
+			}
+		}
+	}
+	return content, nil
 }
 
 //Applies an operation to this document. Version argument indicates what
@@ -37,41 +76,20 @@ func (doc *Document) ApplyToVersion(op Operation, version int) (err error) {
 			op = op.transform(top)
 		}
 	}
-	content := doc.content
-	for c := 0; c < len(op); c++ {
-		comp := op[c]
-		if comp.Si != "" {
-			index := comp.position()
-			str, err := content.get(comp.Path[:len(comp.Path)-1])
-			if err != nil {
-				return InvalidComponentError{msg: str}
-			}
-			str = str[:index] + comp.Si + str[index:]
-			content.set(comp.Path[:len(comp.Path)-1], str)
-		}
-		if comp.Sd != "" {
-			str, err := content.get(comp.Path[:len(comp.Path)-1])
-			if err != nil {
-				return err
-			}
-			str_length := len(comp.Sd)
-			index := comp.position()
-			deleted := str[index : index+str_length]
-			if deleted != comp.Sd {
-				return InvalidComponentError{"Trying to delete '" + comp.Sd + "' but found '" + deleted + "' instead"}
-			}
-			new_str := str[:index] + str[index+str_length:]
-			content.set(comp.Path[:len(comp.Path)-1], new_str)
-		}
-	}
 	doc.ops = append(doc.ops, op)
 	return nil
 }
 
+
+
 //In order to access portions of the document. path is the list of keys in
 //descending order to access final string.
 func (doc *Document) Get(path []string) (inner string, err error) {
-	return doc.content.get(path)
+	result, err := doc.Snapshot()
+	if (err != nil) {
+		return "", err
+	}
+	return result.get(path)
 }
 
 //Applies an operation to a document. This one will apply only local ops to
